122de202626f12bd4056505d1261dbe6729a1374
Aazain <aazainkhan@gmail.com>
2025-02-12 11:03:52 +0400

    page background

--- valentine/src/App.jsx
+++ valentine/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 
 function App() {
   return (
-    <>
-    <h1>Happy Valentine's Day!</h1>
-    </>
+    <div className="wrapper">
+      <div className="heart x1"></div>
+      <div className="heart x2"></div>
+      <div className="heart x3"></div>
+      <div className="heart x4"></div>
+      <div className="heart x5"></div>
+      <div className="altheart x6"></div>
+    </div>
   )
 }
 
